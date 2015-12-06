d4a02e66044e67a7bf712bfdb6d757a2550d1f1b
cecchigiammi <giammarco.cecchini@gmail.com>
2015-12-06 20:04:53 +0100

    Normalize Layout by N
    I divided by N both X and Y coordinates of the base stations and the
    radius of the cells.
    
    NxN = # of BS

--- layout.m
+++ layout.m
@@ -1,23 +1,22 @@
 % Generate hexagonal grid
-N = 21;
+N = 19;
 Rad3Over2 = sqrt(3) / 2;
 [X, Y] = meshgrid(0:1:N);
 n = size(X,1);
-X = Rad3Over2 * X;
-Y = Y + repmat([0 0.5],[n,n/2]);
+X = (Rad3Over2 * X)/N;
+Y = (Y + repmat([0 0.5],[n,n/2]))/N;
 
 % Plot the hexagonal mesh, including cell borders
 [XV, YV] = voronoi(X(:),Y(:)); 
 hold on
 plot(X,Y,'.',XV,YV,'b-')
-axis equal, axis([10 50 10 50]), zoom on
+axis equal, axis([0.1 1 0.1 1]), zoom on
 
-radius = 1/sqrt(3);
+radius = (1/sqrt(3))/N;
 
-%Plot N*N circles centered in X,Y coordinates
+% Plot N*N circles centered in X,Y coordinates
 for i=1:N
     for j=1:N
         circle(X(i,j),Y(i,j),radius)
     end
 end
-
