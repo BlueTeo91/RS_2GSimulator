9a0380b81c0987df6939862f14c54d349ddcc0c9
cecchigiammi <giammarco.cecchini@gmail.com>
2015-12-06 11:16:01 +0100

    Removed "hold on" from random.m function
    Otherwise if you launch multiple times the function it would keep also
    the previous points in the figure.

--- random.m
+++ random.m
@@ -1,6 +1,6 @@
 function p = random(a,b,n)
 % Plot n random points with coordinates (x,y) ranging from a to b
-hold on
+
 % Generate n random points in the 2 coordinate system 
 x = a + (b - a) * rand(1, n); 
 y = a + (b - a) * rand(1, n); 
