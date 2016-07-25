f02556b2c5b17b439d902f89bfbf8c2cca1ab35f
cecchigiammi <giammarco.cecchini@gmail.com>
2016-07-25 11:17:13 +0200

    Add print to video and correct columns in matrices

--- simulator2GKPIs.m
+++ simulator2GKPIs.m
@@ -11,6 +11,12 @@ tic          % Start stopwatch
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Simulator Parameters
 
+% Number of Snapshots
+snap_number = 1;
+
+% Number of Snapshots between two new MS deployments
+MS_update = 10;
+
 % Antennas Parameters
 Ptmax_BS = 10;                                     % BS Max TX Power (Watts)
 Ptmin_BS = 0.01;                                   % BS Min TX Power (Watts)
@@ -74,14 +80,13 @@ SNR_FT_Thr_DL = computeSNR(Prmin_MS_dBm,F_MS,Rb) - 3;     % FT SNR Downlink (dB)
 SNR_FT_Thr_UL = computeSNR(Prmin_BS_dBm,F_BS,Rb) - 3;     % FT SNR Uplink (dB)
 SIR_FT_Thr = 5;                                           % FT SIR (dB)
 
-%KPIs initialization
+% KPIs initialization
 network_load_TOT = 0;
 refCell_load_TOT = 0;
 blocking_rate_TOT = 0;
 refCell_blocking_rate_TOT = 0;
 outage_rate_TOT = 0;
 forced_termination_rate_TOT = 0;
-snap_number = 1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Base Stations Deployment
@@ -103,26 +108,35 @@ scale = R/r;                                       % Scaling Factor
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Mobile Stations Deployment
+
 for snap = 1:snap_number
-    if ((snap == 1) || (mod(snap,10) == 0))        
+    
+    % Print to video
+    fprintf('Snapshot %d/%d\n',snap,snap_number);
+    
+    if ((snap == 1) || (mod(snap,MS_update) == 0))
+        
+        % Print to video
+        fprintf('MS Deployment\n');
+        
         % Area of the rectangle (square km)
         Arect = (((0.8508-0.1519)*(0.8421-0.1579))*(scale^2))/(1e6);
         a = r*sqrt(3)/2;                                   % Apothem of the hexagon (plot)
         Aservice = (((((6*r)*a)/2)*N_BS)*(scale^2))/(1e6); % Area of the service area (square km)
         N_MStot = round(N_MSe*(Arect/Aservice));           % Number of MS in the rectangular area
-
+        
         % MS coordinates generation
         [X_MS,Y_MS] = uniformMS(0.1519,0.8508,0.1579,0.8421,N_MStot);
-
+        
         % Assign nearest BS to each MS and compute distance
         [cellID,shortest_distance] = dsearchn([X_BS,Y_BS],delaunayn([X_BS,Y_BS]),[X_MS,Y_MS]);
-
+        
         % MS temporary matrix
         MSCtemp = [X_MS, Y_MS, cellID, shortest_distance*scale];
         MSindex = find(MSCtemp(:,4) < R);                  % Index of MSCtemp with distance less than cell radius
         MSC = MSCtemp(MSindex,1:2);                        % Save in MSC only the MS inside the service area
         N_MS = length(MSC);                                % Real Number of MS in the service area
-
+        
         % Compute distance between each MS and each BS
         distance = zeros(N_MS,N_BS);
         for i = 1:N_MS
@@ -130,27 +144,27 @@ for snap = 1:snap_number
                 distance(i,j) = (computeDistance(MSC(i,:),BSC(j,:)).*scale);
             end
         end
-
+        
         % Sort distance matrix in ascending order
         [neighbouring_distance, neighbouring_index] = sort(distance,2);
-
+        
         % Add neighbouring cell ID and distances to MS matrix
-        MSC = [MSC, neighbouring_index(:,1:4), neighbouring_distance(:,1:4)];
-
+        MSC = [MSC(:,1:2), neighbouring_index(:,1:4), neighbouring_distance(:,1:4)];
+        
         %plotMS(MSC(:,1),MSC(:,2));                         % Plot MS Deployment
     end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Traffic Generation
-
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Traffic Generation
+    
     % Probability of being in a call between [PcallMin, PcallMax]
     Pcall = abs(Pcall_average+Pcall_StDev*randn(1,1));
-
+    
     % Generate random vector of 0s and 1s -> 1 = calling, 0 = not calling
     % (with a percentage of Pcall users calling)
     calling = rand(N_MS,1) <= Pcall;
     N_MScalling = sum(calling);                        % Number of calling MS
-    MSC = [MSC, calling, zeros(N_MS,1)];               % Add call state column to MS matrix
-
+    MSC = [MSC(:,1:10), calling, zeros(N_MS,1)];       % Add call state column to MS matrix
+    
     % Assign traffic type based on probabilities defined above
     % 1 = DOWNLINK
     % 2 = UPLINK
@@ -158,14 +172,14 @@ for snap = 1:snap_number
     j = 1:N_MScalling;
     i = find(calling);
     MSC(i,12) = traffic_kind(j);                       % Add traffic type column to MS matrix
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Propagation
-
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Propagation
+    
     % Inizialize first column with traffic type
     links(:,1) = MSC(:,12);
     temp_power = zeros(N_MS,4);
-
+    
     % Compute received power for each link (using Beacon Signal)
     for i=1:N_MS
         switch links(i,1)
@@ -177,50 +191,50 @@ for snap = 1:snap_number
                 temp_power(i,:) = propagation(Ptmax_BS_dBm,fc,hBS,sigmadB,MSC(i,7:10));
         end
     end
-
+    
     % Sort power in descending order and correspondent BSid
     temp_BSid = MSC(:,3:6);
     [temp_power,power_index] = sort(temp_power,2,'descend');
     for i = 1:N_MS
         temp_BSid(i,:) = temp_BSid(i,power_index(i,:));
     end
-
+    
     % Add BSid and power ordered by received power to links matrix
-    links = [links, temp_BSid, temp_power];
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% RUs Assignment
-
+    links = [links(:,1), temp_BSid, temp_power];
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% RUs Assignment
+    
     N_RU_cellDL = round((N_RU/K)/2);           % Number of RRUs for DL per cell
     N_RU_cellUL = round((N_RU/K)/2);           % Number of RRUs for UL per cell
-
+    
     % Add number of available RUs DL and RUs UL to BSC matrix
-    BSC = [BSC, N_RU_cellDL*ones(N_BS,1), N_RU_cellUL*ones(N_BS,1)];
+    BSC = [BSC(:,1:2), N_RU_cellDL*ones(N_BS,1), N_RU_cellUL*ones(N_BS,1)];
     N_RU_tot = sum(BSC(:,3)) + sum(BSC(:,4));
     N_RU_cell = N_RU_cellDL + N_RU_cellUL;
-
+    
     % Admission Control - Directed Retry
-
+    
     % Scheduling: First-Come-First-Served (FCFS)
     % MSid ordered by arrival
-
+    
     % Links column 10:
     %    0 -> RU not requested
     %   -1 -> RU not assigned (blocked)
     % BSid -> RU assigned by BSid
-
+    
     % Links column 11:
     %      0 -> RU not assigned
     % RUs ID -> number of the RU
-
+    
     % Links column 12:
     % 0 -> MS not connected
     % 1 -> MS connected to the first best server
     % 2 -> MS connected to the second best server
     % 3 -> MS connected to the third best server
     % 4 -> MS connected to the fourth best server
-
-    links = [links, zeros(N_MS,1), zeros(N_MS,1), zeros(N_MS,1)];
+    
+    links = [links(:,1:9), zeros(N_MS,1), zeros(N_MS,1), zeros(N_MS,1)];
     for i = 1:N_MS
         switch links(i,1)
             case 1      % Downlink
@@ -275,12 +289,12 @@ for snap = 1:snap_number
                 links(i,10) = 0;         % RU not requested
         end
     end
-
+    
     N_RU_available = sum(BSC(:,3)) + sum(BSC(:,4));
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Power Control
-
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Power Control
+    
     connected_MSid = find(links(:,11)>0);
     Pt_dBm = zeros(N_MS,1);
     Pr_dBm = zeros(N_MS,1);
@@ -316,26 +330,26 @@ for snap = 1:snap_number
                 Pr_dBm(i,1) = links(i,9);
         end
     end
-
+    
     connection_type = links(connected_MSid,1);
     connected_BSid = links(connected_MSid,10);
     RUid = links(connected_MSid,11);
     Pt_dBm = Pt_dBm(connected_MSid,1);
     Pr_dBm = Pr_dBm(connected_MSid,1);
-
+    
     % Signal Based Power Control (PC)
     PtPC_dBm = zeros(length(connected_MSid),1);
     PrPC_dBm = zeros(length(connected_MSid),1);
     for i=1:length(connected_MSid)
         [PtPC_dBm(i,1), PrPC_dBm(i,1)] = powercontrol(connection_type(i,1),Pt_dBm(i,1),Pr_dBm(i,1),Prmin_BS_dBm,Prmin_MS_dBm,Ptmax_BS_dBm,Ptmin_BS_dBm,Ptmax_MS_dBm,Ptmin_MS_dBm,PCmargin_dB,delta);
     end
-
+    
     % Create connected_links matrix with the following columns
     connected_links = [connection_type, connected_MSid, connected_BSid, RUid, PtPC_dBm, PrPC_dBm];
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% SNR Computation
-
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% SNR Computation
+    
     % Compute SNR for each connected link
     SNR_dB = zeros(length(connected_MSid),1);
     for i=1:length(connected_MSid)
@@ -346,26 +360,26 @@ for snap = 1:snap_number
                 SNR_dB(i,1) = computeSNR(connected_links(i,6),F_BS,Rb);
         end
     end
-
+    
     % Add SNR (dB) column in connected_links matrix
-    connected_links = [connected_links, SNR_dB];
-
+    connected_links = [connected_links(:,1:6), SNR_dB];
+    
     % Add state column
     % Active = 1
     % Silent = 0
     state = rand(length(connected_links),1) <= 0.45;
-    connected_links = [connected_links, state];
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% SIR Computation
-
+    connected_links = [connected_links(:,1:7), state];
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% SIR Computation
+    
     % Search cell ID of two interfering tiers
     interfering_cellid(:,1) = K+1:K:N_BS;
-
+    
     % SIR DOWNLINK
     MS_DLrefCellindex = find(connected_links(:,3)==1 & connected_links(:,1)==1 & connected_links(:,8)==1);
     MS_DLrefCell = [connected_links(MS_DLrefCellindex,2),connected_links(MS_DLrefCellindex,4),zeros(length(MS_DLrefCellindex),1),zeros(length(MS_DLrefCellindex),1)];
-
+    
     for i = 1:length(MS_DLrefCellindex)
         I = 0;
         for j = 1:length(interfering_cellid)
@@ -382,11 +396,11 @@ for snap = 1:snap_number
         MS_DLrefCell(i,3) = SIR_dB;
         MS_DLrefCell(i,4) = connected_links(MS_DLrefCellindex(i,1),7);
     end
-
+    
     % SIR UPLINK
     MS_ULrefCellindex = find(connected_links(:,3)==1 & connected_links(:,1)==2 & connected_links(:,8)==1);
     MS_ULrefCell = [connected_links(MS_ULrefCellindex,2),connected_links(MS_ULrefCellindex,4),zeros(length(MS_ULrefCellindex),1),zeros(length(MS_ULrefCellindex),1)];
-
+    
     for i = 1:length(MS_ULrefCellindex)
         I = 0;
         for j = 1:length(interfering_cellid)
@@ -403,37 +417,37 @@ for snap = 1:snap_number
         MS_ULrefCell(i,3) = SIR_dB;
         MS_ULrefCell(i,4) = connected_links(MS_ULrefCellindex(i,1),7);
     end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% KPIs Computation
-
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% KPIs Computation
+    
     % Network Load Percentage (based on assigned RUs)
     network_load = ((N_RU_tot - N_RU_available)/N_RU_tot)*100;
     % Incremental sum of network_load for KPI computation
     network_load_TOT = network_load_TOT + network_load;
-
+    
     % Reference Cell Load Percentage (BSid=1)
     refCell_load = ((N_RU_cell - (BSC(1,3) + BSC(1,4)))/N_RU_cell)*100;
     % Incremental sum of refCell_load for KPI computation
     refCell_load_TOT = refCell_load_TOT + refCell_load;
-
+    
     % Blocking Rate (%)
     if K==7
         border_cellsID = [51,52,63,58,59,60,65,66,67,72,73,74,75,80,81,82,87,88,89,90,95,96,97,102,103,104,105,110,11,112,117,118,119,114,125,126,121,132,133,128,129,56];
     end
-
+    
     for i = 1:length(border_cellsID)
         border_links_index = find(links(:,10)==border_cellsID(i));
         links(border_links_index,:) = [];
     end
-
+    
     N_MSblocked = length(find(links(:,10)==-1));
     N_MSdownlink = length(find(links(:,1)==1));
     N_MSuplink = length(find(links(:,1)==2));
     blocking_rate = (N_MSblocked/(N_MSdownlink + N_MSuplink))*100;
     % Incremental sum of blocking_rate for KPI computation
     blocking_rate_TOT = blocking_rate_TOT + blocking_rate;
-
+    
     % Reference Cell Blocking Rate (%) (BSid=1)
     [row,column] = find(links(:,2:5)==1);
     index_temp = sort(row);
@@ -443,7 +457,7 @@ for snap = 1:snap_number
     refCell_blocking_rate = (N_MSblocked_refCell / (N_MSblocked_refCell + (N_RU_cell - BSC(1,3) + BSC(1,4))))*100;
     % Incremental sum of refCell_blocking_rate for KPI computation
     refCell_blocking_rate_TOT = refCell_blocking_rate_TOT + refCell_blocking_rate;
-
+    
     % Outage Rate (%) (BSid=1)
     N_MS_Out_DL = 0;
     for i = 1:length(MS_DLrefCell)
@@ -457,11 +471,11 @@ for snap = 1:snap_number
             N_MS_Out_UL = N_MS_Out_UL+1;
         end
     end
-
+    
     outage_rate = ((N_MS_Out_DL+N_MS_Out_UL)/(length(MS_DLrefCell)+length(MS_ULrefCell)))*100;
     % Incremental sum of outage_rate for KPI computation
     outage_rate_TOT = outage_rate_TOT + outage_rate;
-
+    
     % Forced Termination Rate (%) (BSid=1)
     N_MS_FT_DL = 0;
     for i = 1:length(MS_DLrefCell)
@@ -475,14 +489,16 @@ for snap = 1:snap_number
             N_MS_FT_UL = N_MS_FT_UL+1;
         end
     end
-
+    
     forced_termination_rate = ((N_MS_FT_DL+N_MS_FT_UL)/(length(MS_DLrefCell)+length(MS_ULrefCell)))*100;
     % Incremental sum of outage_rate for KPI computation
     forced_termination_rate_TOT = forced_termination_rate_TOT + forced_termination_rate;
-
+    
 end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Total KPIs computation    
+%% Total KPIs computation & Print to file
+
 network_load_TOT = network_load_TOT / snap_number;
 refCell_load_TOT = refCell_load_TOT / snap_number;
 blocking_rate_TOT = blocking_rate_TOT / snap_number;
@@ -490,8 +506,11 @@ refCell_blocking_rate_TOT = refCell_blocking_rate_TOT / snap_number;
 outage_rate_TOT = outage_rate_TOT / snap_number;
 forced_termination_rate_TOT = forced_termination_rate_TOT / snap_number;
 
-fileID = fopen('KPIs.txt','a');
-fprintf(fileID,'Network Load: %.4f\nReference Cell Load: %.4f\nBlocking Rate: %.4f\nReference Cell Blocking Rate: %.4f\nOutage Rate: %.4f\nForced Termination Rate: %.4f\n',network_load_TOT,refCell_load_TOT,blocking_rate_TOT,refCell_blocking_rate_TOT,outage_rate_TOT,forced_termination_rate_TOT);
+fileID = fopen('KPIs.txt','at');
+fprintf(fileID,'%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4t\t%.4f\t%.4f\n',delta,PCmargin_dB,network_load_TOT,refCell_load_TOT,blocking_rate_TOT,refCell_blocking_rate_TOT,outage_rate_TOT,forced_termination_rate_TOT);
 fclose('all');
+
+% Print to video
+fprintf('PC Parameters: delta = %.4f\tMargin = %d dB\nNetwork Load: %.4f\nReference Cell Load: %.4f\nBlocking Rate: %.4f\nReference Cell Blocking Rate: %.4f\nOutage Rate: %.4f\nForced Termination Rate: %.4f\n',delta,PCmargin_dB,network_load_TOT,refCell_load_TOT,blocking_rate_TOT,refCell_blocking_rate_TOT,outage_rate_TOT,forced_termination_rate_TOT);
 
 toc          % Stop stopwatch
